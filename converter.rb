e0055109d982f4f96fdbee2579461e74409273d0
S.H <gamelinks007@gmail.com>
2019-11-05 23:47:45 +0900

    Update converter.rb

--- converter.rb
+++ converter.rb
@@ -1,10 +1,12 @@
 
 File.open("text.txt", "r+"){|f|
-    f.each_with_index{|line, index|
-        line.gsub!(/━+|-+/){|c| ";" + c}
-        line.gsub!(/。/, "。[l]")
-        line.gsub!(/。$/, "。[p]")
-        line.gsub!(/」/, "」[p]")
-        puts line
+    File.open("convert.txt", "w+"){|w|
+        f.each_with_index{|line, index|
+            line.gsub!(/━+|-+/){|c| ";" + c}
+            line.gsub!(/。/, "。[l]")
+            line.gsub!(/。$/, "。[p]")
+            line.gsub!(/」/, "」[p]")
+            w.puts(line)
+        }
     }
 }
